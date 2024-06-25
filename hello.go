72d22863ff53d98574dcf4afad8d3cb8943673c6
Yousef Moustafa <ymoustafa005@gmail.com>
2024-06-25 13:21:25 -0400

    Go Tutorial: Go Slices

--- hello.go
+++ hello.go
@@ -11,10 +11,10 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Yousef")
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	fmt.Println(message)
 }
